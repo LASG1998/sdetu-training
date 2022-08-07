41c7aad7b37f70ff8d2f8b5bef12eefe15e11db8
LASG1998 <110633403+LASG1998@users.noreply.github.com>
2022-08-07 13:26:13 -0500

    modified hellowordApp

--- src/basics/helloWorld.java
+++ src/basics/helloWorld.java
@@ -1,8 +1,11 @@
 package basics;
 
+import java.awt.Image;
+
 public class helloWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
+		System.out.println("adding code to my project");
 	}
 
 }
